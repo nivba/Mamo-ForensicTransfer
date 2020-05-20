eaec6a3248bdd9b64d48172241fabd91920e8396
Niv Bar-on <62094356+nivba@users.noreply.github.com>
2020-05-20 14:09:03 +0300

    Add files via upload

---
+++ procedures/DataLoder.py
@@ -0,0 +1,79 @@
+
+import numpy as np
+import os
+
+from matplotlib import pyplot as plt
+
+class data_loder:
+    def __init__(self, path):
+        self.path = path
+        self.real = []
+        self.fake = []
+        self.Tile_size = 256
+        self. Tile_resize = 64
+
+    def load(self):
+        for img in os.listdir(self.path):
+            img_name = img[0:-4]
+            if img_name[-3:len(img_name)] == "map":
+                continue
+            #print(img_name)
+            im_path = os.path.join(self.path, img)
+            img_array = np.load(im_path)
+            map_path = os.path.join(self.path,img_name+"_map.npy")
+            map_array = np.load(map_path)
+            self.break_to_tiles(img_array, map_array)
+        self.real = np.array(self.real)
+        self.fake = np.array(self.fake)
+        print(str(len(self.real))+" real tiles")
+        print(str(len(self.fake))+" fake tiles")
+
+    def break_to_tiles(self, img_array, map_array):
+        rows, cols = img_array.shape
+        i = 0
+        while i+self.Tile_size <= rows:
+            j = 0
+            while j+self.Tile_size <= cols:
+                im_tile = img_array[i:i+self.Tile_size, j:j+self.Tile_size]
+                if not self.is_mammo(im_tile):
+                    j = j + int(max(min(self.Tile_size / 2, cols - self.Tile_size - j),1))
+                    continue
+                map_tile = map_array[i:i+self.Tile_size, j:j+self.Tile_size]
+                if self.label_tile(map_tile) == 0:
+                    self.real.append(im_tile)
+                else:
+                    self.fake.append(im_tile)
+                j = j + int(max(min(self.Tile_size / 2, cols - self.Tile_size - j),1))
+            i = i + int(max(min(self.Tile_size / 2, rows - self.Tile_size - i),1))
+
+    def is_mammo(self,tile):
+        return np.mean(tile) > 100
+
+    def label_tile(self, map_tile):
+        num_of_pixels = self.Tile_size*self.Tile_size
+        num_of_fake_pixels = np.sum(map_tile)
+        fake_part = num_of_fake_pixels/num_of_pixels
+        if fake_part > 0.02:
+            return 1
+        return 0
+
+    def show_real(self):
+        for i in range(len(self.real)):
+                plt.figure()
+                plt.imshow(self.real[i], cmap="gray")
+                plt.show()
+    def show_fake(self):
+        for i in range(len(self.fake)):
+                plt.figure()
+                plt.imshow(self.fake[i], cmap="gray")
+                plt.show()
+
+    def load_ephoc(self):
+        real_idx = np.random.permutation(len(self.real))
+        fake_idx = np.random.permutation(len(self.fake))
+        return (self.real[real_idx, :, :]/1100).astype(np.float32), (self.fake[fake_idx, :, :]/1100).astype(np.float32)
+
+#loader = data_loder("C:\\niv\\Mammo_GAN\\injected")
+#loader.load()
+#print(len(loader.data))
+#print(sum(loader.labels))
---
+++ procedures/trainer.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import DataLoder
+import numpy as np
+
+class Mammo_FT(nn.Module):
+    def __init__(self):
+        super(Mammo_FT, self).__init__()
+        self.layer_d1 = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+        self.layer_d2 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=3 ,stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
+        self.layer_d3 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.layer_d4 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.layer_d5 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.layer_u5 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.layer_u4 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.layer_u3 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
+        self.layer_u2 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+        self.layer_u1 = nn.Sequential(
+            nn.Conv2d(8, 1, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(1),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        latent = self.layer_d1(x)
+        latent = self.layer_d2(latent)
+        latent = self.layer_d3(latent)
+        latent = self.layer_d4(latent)
+        latent = self.layer_d5(latent)
+        C1 = latent[:, 0:64, :, :]
+        C0 = latent[:, 64:128, :, :]
+        S1 = C1.sum(dim=1).sum(dim=1).sum(dim=1)
+        S0 = C0.sum(dim=1).sum(dim=1).sum(dim=1)
+        a1 = torch.div(S1, S1 + S0)
+        a1_arr = a1.detach().numpy()
+        for idx, a1_score in enumerate(a1_arr, start=0) :
+            if a1_score > 0.5:
+                latent[idx, 0:64, :, :] = torch.zeros((1, 64, 16, 16))
+            else:
+                latent[idx, 64:128, :, :] = torch.zeros((1, 64, 16, 16))
+        reconstruct = self.layer_u5(latent)
+        reconstruct = self.layer_u4(reconstruct)
+        reconstruct = self.layer_u3(reconstruct)
+        reconstruct = self.layer_u2(reconstruct)
+        reconstruct = self.layer_u1(reconstruct)
+        return a1, reconstruct
+
+def train(model, data_path, ephocs=1, batch_size=1, learning_rate=0.002):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    model.to(device)
+    optimaizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    data_loader = DataLoder.data_loder(data_path)
+    data_loader.load()
+    training_set_size = min(len(data_loader.fake), len(data_loader.real))
+    n_batch = (training_set_size//batch_size)*2
+    for ephoc in range(ephocs):
+        real_ephoc, fake_ephoc = data_loader.load_ephoc()
+        real_ephoc = np.expand_dims(real_ephoc, axis=1)
+        fake_ephoc = np.expand_dims(fake_ephoc, axis=1)
+        for batch in range(n_batch):
+            if batch % 2 == 0:
+                img_batch = real_ephoc[(batch//2)*batch_size:((batch//2)+1)*batch_size,:,:,:]
+                label_batch = torch.from_numpy(np.zeros(batch_size).astype(np.float32)).to(device)
+            else:
+                img_batch = fake_ephoc[(batch // 2) * batch_size:((batch // 2) + 1) * batch_size, :, :, :]
+                label_batch = torch.from_numpy(np.ones(batch_size).astype(np.float32)).to(device)
+            img_batch = torch.from_numpy(img_batch).to(device)
+            # forward pass
+            out_labels, out_images = model(img_batch)
+            loss = 0.1*nn.functional.l1_loss(out_images, img_batch) +\
+                   nn.functional.binary_cross_entropy(out_labels, label_batch)
+            # backward
+            optimaizer.zero_grad()
+            loss.backward()
+            optimaizer.step()
+            print("Ephoc: %d/%d, batch: %d/%d, loss = %1.4f, true_label = %d, mean_label = %1.4f" %
+                  (ephoc+1,ephocs,batch+1,n_batch,loss.item(),batch % 2,np.mean(out_labels.detach().numpy())))
+
+data_path = "C:\\niv\\Mammo_GAN\\training set"
+model = Mammo_FT()
+train(model, data_path, ephocs=5, batch_size=2)
